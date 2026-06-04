88d668e0ef4805eb0f50add755acd5ed4b2acb94
Jonong03-umn <jon.ong03@gmail.com>
2026-06-04 17:55:25 +0800

    Update UseCase3_Rsquared_working.R

--- UseCase3_Rsquared_working.R
+++ UseCase3_Rsquared_working.R
@@ -9,6 +9,144 @@ source("/Users/jonong/Library/CloudStorage/OneDrive-Personal/Documents/1- Projec
 #source_python("/Users/jonong/Library/CloudStorage/OneDrive-Personal/Documents/1- Projects/PerturbR-CDA/process1.py")
 #source_python("/Users/jonong/Library/CloudStorage/OneDrive-Personal/Documents/1- Projects/PerturbR-CDA/boss_py.py")
 
+{
+  gen_AR1 <- function(p, rho = 0.6){
+    # check if rank deficient: kappa value, SVD decomposition and eigenvalues not meeting tolerance level?
+    
+    Rxx <- matrix(NA,p,p)
+    for(i in 1:p){
+      for (j in 1:p){
+        if(i == j) Rxx[i,j]<- 1
+        if(i != j)  Rxx[i,j] <- rho^(abs(i-j))
+      }
+    }
+    return(Rxx)
+    
+  }
+  computeRsq <- function(M){
+    # M is the correlation matrix of (Y,X)
+    p = ncol(M)
+    Rxy = as.matrix(M[2:p,1],ncol=1)
+    Rxx = M[2:p, 2:p]
+    
+    Rsq = t(Rxy) %*% solve(Rxx) %*% Rxy
+    return(Rsq)
+  }
+  
+  make_mcmc<- function(S, asy.n, NCHAIN=1000, init= S, alpha=0.05, 
+                       scale.init= 1, adaptive= TRUE, M= 100, acceptance.target = 0.44, 
+                       seed= 123){
+    p<- ncol(S)    #p: dimension of S (correlation matrix)
+    d<- p*(p-1)/2  #d: dimension of correlation space
+    accept <- 0
+    q<- 0         # to trace scale_factor
+    
+    trace_scale<- array(NA_real_, dim=c(NCHAIN))
+    scale_factor<- scale.init
+    set.seed(seed)
+    
+    RCHAIN<- array(NA, dim= c(p,p,NCHAIN))
+    RCHAIN[,,1] <- init
+    
+    for (i in 2:NCHAIN){
+      
+      Rcurrent<- RCHAIN[,,i-1]
+      rcurrent<- Rcurrent[lower.tri(Rcurrent)]
+      
+      # Step 1: Draw R* (named as Rs)
+      Psi_Rcurrent<- metaSEM::asyCov(Rcurrent, n = asy.n)
+      
+      Rs<- rwaldcloud(Rcurrent, Psi= scale_factor*Psi_Rcurrent, B = 1) %>% drop()
+      rs<- Rs[lower.tri(Rs)]
+      
+      Psi_Rs<- metaSEM::asyCov(Rs, n= asy.n)
+      # Step 2: Acceptance Ratio
+      partA<- dmvnorm(x= rcurrent, mean = rs, sigma= Psi_Rs, log= T)
+      partB<- dmvnorm(x= rs, mean= rcurrent, sigma= Psi_Rcurrent, log=T)
+      logMH = partA - partB 
+      
+      partC<- wald.test(Rpop=Rs, Rsample= S, alpha= alpha, asy.n= asy.n)  # Rstar is at the center
+      IR   <- partC$pval > alpha
+      
+      # Step 3: accept or reject
+      if(IR==TRUE && logMH > log(runif(1))){ # accept Rs
+        RCHAIN[,,i]<- Rs
+        accept <- accept + 1
+      } else {
+        RCHAIN[,,i]<- Rcurrent
+      }
+      
+      a_rate = accept/i
+      cat("Acceptance Rate: ", round(a_rate,3),"\n")
+      
+      if(adaptive==TRUE){
+        # update scale parameter every M steps
+        ### If acceptance rate > 44% (acceptance.target parameter) 
+        ############################ (step size is too small, then update scale to min(acceptance, 0.75)/ 0.44)
+        ### If acceptance rate < 44% (step size is too big, then update scale to max(acceptance, 0.20)/ 0.44)
+        ##### M: adaptive block
+        if(i/M == floor(i/M)){
+          a_rate=ifelse(a_rate>.75,.75,ifelse(a_rate<.1,.1,a_rate))
+          scale_factor=scale_factor*a_rate/acceptance.target
+          # cat("Scale Factor: ", scale_factor, "\n")
+        }
+      }
+      trace_scale[i]<- scale_factor
+    }
+    attr(RCHAIN, "scale_trace") <- trace_scale
+    
+    return(RCHAIN)
+  }
+  
+  simdriver<- function(Rxx, beta, k=0.85, ss=100, nchain = 1000, alpha = 0.05){
+    
+    p = ncol(Rxx)
+    
+    # Step 3: Compute S
+    S = t(beta) %*% Rxx %*% (beta)
+    
+    # Step 4: Compute sigma2
+    sigma2 = S *(1- k)/k
+    
+    # Step 5: Generate X ~ N(0, RXX) with sample size ss
+    X <- rmvnorm(n = ss, mean = rep(0,p), sigma = Rxx)
+    
+    # Step 6: Generate Y = X'beta + eps where eps ~ N(0, sigma2)
+    eps <- rnorm(n=ss, mean = 0, sd = sqrt(sigma2))
+    Y <- X %*% (beta) + eps
+    dt<- cbind(Y,X)
+    
+    # Step 7: estimate sample cor Rhat
+    Rhat = cor(cbind(Y, X))
+    
+    # Step 8: map cor R to Rsq
+    khat<- computeRsq(Rhat)
+    
+    # Step 9: Generate perturbation cloud
+    RCHAIN <- make_mcmc(S= Rhat, asy.n= ss, NCHAIN = nchain, alpha = alpha)
+    
+    # Step 10: map RCHAIN to RSQ.hat.chain
+    khat.chain <- sapply(1:nchain, function(i){
+      computeRsq(RCHAIN[,,i])
+    })
+    
+    khat.boot <- sapply(1:nchain, function(i){
+      boot.id <-  sample(ss, ss, replace=TRUE)
+      cor.boot<- cor(dt[boot.id,])
+      computeRsq(cor.boot)
+    })
+    
+    return(list(
+      khat.point = khat,
+      khat.chain = khat.chain,
+      khat.boot = khat.boot
+    ))
+  }
+  
+  
+}
+
+
 # Step 0: Specify parameters
 p<- 5
 Rsq <- 0.85
@@ -17,19 +155,6 @@ nchain = 20000
 
 
 # Step 1: construct an AR1 Rxx corr structure 
-gen_AR1 <- function(p, rho = 0.6){
-  # check if rank deficient: kappa value, SVD decomposition and eigenvalues not meeting tolerance level?
-  
-  Rxx <- matrix(NA,p,p)
-  for(i in 1:p){
-    for (j in 1:p){
-      if(i == j) Rxx[i,j]<- 1
-      if(i != j)  Rxx[i,j] <- rho^(abs(i-j))
-    }
-  }
-  return(Rxx)
-  
-}
 Rxx<- gen_AR1(p=p, rho = 0.8)
 Rxx
 kappa(Rxx)  # should be fine as long as rho is not extremely large. Also set rho to large value so we can also study performance at the boundary 
@@ -58,82 +183,9 @@ Rxy = matrix(cor(Y, X), ncol = 1)
 Rhat
 
 # Step 8: map cor R to Rsq
-computeRsq <- function(M){
-  # M is the correlation matrix of (Y,X)
-  p = ncol(M)
-  Rxy = as.matrix(M[2:p,1],ncol=1)
-  Rxx = M[2:p, 2:p]
-  
-  Rsq = t(Rxy) %*% solve(Rxx) %*% Rxy
-  return(Rsq)
-}
 computeRsq(Rhat)
 
 # Step 9: Generate perturbation cloud
-make_mcmc<- function(S, asy.n, NCHAIN=1000, init= S, alpha=0.05, 
-                     scale.init= 1, adaptive= TRUE, M= 100, acceptance.target = 0.44, 
-                     seed= 123){
-  p<- ncol(S)    #p: dimension of S (correlation matrix)
-  d<- p*(p-1)/2  #d: dimension of correlation space
-  accept <- 0
-  q<- 0         # to trace scale_factor
-  
-  trace_scale<- array(NA_real_, dim=c(NCHAIN))
-  scale_factor<- scale.init
-  set.seed(seed)
-  
-  RCHAIN<- array(NA, dim= c(p,p,NCHAIN))
-  RCHAIN[,,1] <- init
-  
-  for (i in 2:NCHAIN){
-    
-    Rcurrent<- RCHAIN[,,i-1]
-    rcurrent<- Rcurrent[lower.tri(Rcurrent)]
-    
-    # Step 1: Draw R* (named as Rs)
-    Psi_Rcurrent<- metaSEM::asyCov(Rcurrent, n = asy.n)
-    
-    Rs<- rwaldcloud(Rcurrent, Psi= scale_factor*Psi_Rcurrent, B = 1) %>% drop()
-    rs<- Rs[lower.tri(Rs)]
-    
-    Psi_Rs<- metaSEM::asyCov(Rs, n= asy.n)
-    # Step 2: Acceptance Ratio
-    partA<- dmvnorm(x= rcurrent, mean = rs, sigma= Psi_Rs, log= T)
-    partB<- dmvnorm(x= rs, mean= rcurrent, sigma= Psi_Rcurrent, log=T)
-    logMH = partA - partB 
-    
-    partC<- wald.test(Rpop=Rs, Rsample= S, alpha= alpha, asy.n= asy.n)  # Rstar is at the center
-    IR   <- partC$pval > alpha
-    
-    # Step 3: accept or reject
-    if(IR==TRUE && logMH > log(runif(1))){ # accept Rs
-      RCHAIN[,,i]<- Rs
-      accept <- accept + 1
-    } else {
-      RCHAIN[,,i]<- Rcurrent
-    }
-    
-    a_rate = accept/i
-    cat("Acceptance Rate: ", round(a_rate,3),"\n")
-    
-    if(adaptive==TRUE){
-      # update scale parameter every M steps
-      ### If acceptance rate > 44% (acceptance.target parameter) 
-      ############################ (step size is too small, then update scale to min(acceptance, 0.75)/ 0.44)
-      ### If acceptance rate < 44% (step size is too big, then update scale to max(acceptance, 0.20)/ 0.44)
-      ##### M: adaptive block
-      if(i/M == floor(i/M)){
-        a_rate=ifelse(a_rate>.75,.75,ifelse(a_rate<.1,.1,a_rate))
-        scale_factor=scale_factor*a_rate/acceptance.target
-        # cat("Scale Factor: ", scale_factor, "\n")
-      }
-    }
-    trace_scale[i]<- scale_factor
-  }
-  attr(RCHAIN, "scale_trace") <- trace_scale
-  
-  return(RCHAIN)
-}
 
 RCHAIN <- make_mcmc(S= Rhat, asy.n= ss, NCHAIN = nchain)
 RCHAIN %>% dim
@@ -170,50 +222,6 @@ range(Rsq.hat.chain) %>% diff
 # Output: 1) point estimate of Rsq, 2) range estimates (eg 95% CI) of Rsq based on mcmc samples
 # Notation: correlation matrix as M, Rsquared as k
 
-simdriver<- function(Rxx, beta, k=0.85, ss=100, nchain = 1000, alpha = 0.05){
-  
-  p = ncol(Rxx)
-  
-  # Step 3: Compute S
-  S = t(beta) %*% Rxx %*% (beta)
-  
-  # Step 4: Compute sigma2
-  sigma2 = S *(1- k)/k
-  
-  # Step 5: Generate X ~ N(0, RXX) with sample size ss
-  X <- rmvnorm(n = ss, mean = rep(0,p), sigma = Rxx)
-  
-  # Step 6: Generate Y = X'beta + eps where eps ~ N(0, sigma2)
-  eps <- rnorm(n=ss, mean = 0, sd = sqrt(sigma2))
-  Y <- X %*% (beta) + eps
-  dt<- cbind(Y,X)
-  
-  # Step 7: estimate sample cor Rhat
-  Rhat = cor(cbind(Y, X))
-  
-  # Step 8: map cor R to Rsq
-  khat<- computeRsq(Rhat)
-  
-  # Step 9: Generate perturbation cloud
-  RCHAIN <- make_mcmc(S= Rhat, asy.n= ss, NCHAIN = nchain, alpha = alpha)
-
-  # Step 10: map RCHAIN to RSQ.hat.chain
-  khat.chain <- sapply(1:nchain, function(i){
-    computeRsq(RCHAIN[,,i])
-  })
-  
-  khat.boot <- sapply(1:nchain, function(i){
-    boot.id <-  sample(ss, ss, replace=TRUE)
-    cor.boot<- cor(dt[boot.id,])
-    computeRsq(cor.boot)
-  })
-  
-  return(list(
-    khat.point = khat,
-    khat.chain = khat.chain,
-    khat.boot = khat.boot
-  ))
-}
 
 p= 8
 rho = 0.6
@@ -293,11 +301,13 @@ out.boot %>% quantile(.,c(0.025, 0.975))
 nvar = 5
 ktrue = 0.9
 nchain = 20000
-B = 300
+B = 5
 alpha = 0.05
 
 rho_ = c(0.2, 0.5, 0.8)
+rho_ = c(0.8)
 ss_ = c(200, 500, 1000)
+ss_ = c(1000)
 
 beta.s1 = rep(0.6, nvar)
 beta.s2 = rep(0.2, nvar)
