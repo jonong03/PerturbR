af10c83a90e14289cc1dad44817adbdcf3333a95
jonong03 <jon.ong03@gmail.com>
2026-09-04 23:52:44 -0400

    Update params.R

--- UseCase/msi/params.R
+++ UseCase/msi/params.R
@@ -6,13 +6,13 @@ nchain <- 3000    # PerturbR samples per Monte Carlo replicate
 
 R2true <- c(0.5)
 phi <- c(0, 0.2, 0.5, 0.8)
-ss <- c(1000, 3000, 5000)
-p <- c(2)
+ss <- c(100, 300, 500, 1000, 3000, 5000)
+p <- c()
 
 beta.list <- c(
   "function(p) rep(0.6, p)",
-  "function(p) rep(0.2, p)",
-  "function(p) rep(c(-0.4, 0.4), length = p)"
+  "function(p) rep(0.2, p)"
+  #"function(p) rep(c(-0.4, 0.4), length = p)"
 )
 
 param.m <- expand.grid(
